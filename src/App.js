fcc7db6908c23fdb623cf7b3bb206fa349c1ba79
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2022-04-27 12:50:04 +0200

    search engine built

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
-      Hi Hi
+      <div className="container">
+        <header className="App-header">Hi Hi</header>
+        <main>
+        
+          <Dictionary defaultKeyword="sunset" />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Lea</small>
+        </footer>
+      </div>
     </div>
   );
 }

fb545ab55067c018a5a9c64aa09cf0bf4d2eee97
Lea Papadopoulos <92717379+LeaPapadopoulos@users.noreply.github.com>
2022-04-27 12:58:05 +0200

    keyword added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,7 +7,7 @@ export default function Dictionary() {
 
     function search(event){
         event.preventDefault();
-        alert ("Searching");
+        alert (`Searching for ${keyword}`);
     }
 
 
